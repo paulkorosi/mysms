ba8bef44b50af59667990086925856cdf17072b9
paulkorosi <76018963+paulkorosi@users.noreply.github.com>
2021-08-02 14:06:55 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mysms::Application.config.secret_token = 'd6c2d873449f5059d2dfdf37ec9f005e464f92e2adeb8925ce57bb79b6fdfb6192d1a2b77626eec34921743a1050b76a735dd9d3a58f22f5b7a8297328b6d908'
