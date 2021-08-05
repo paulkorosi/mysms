5127b1e77edffe0d609f687dcdf325fc42d19a52
paulkorosi <76018963+paulkorosi@users.noreply.github.com>
2021-08-05 15:10:06 +1000

    fix incorrect 'length' syntax in student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   # allow for null values where applicable to allow for single names
   # allow for null values where applicable since you cannot legally force a user to identify their gender
   validates :title, length: { maximum: 10 }
-  validates :last_name, :length: { maximum: 50 } , presence: { message: "Please enter your family name" }
-  validates :first_name, :middle_name, :length: { maximum: 50 }
+  validates :last_name, length: { maximum: 50 } , presence: { message: "Please enter your family name" }
+  validates :first_name, :middle_name, length: { maximum: 50 }
   validates :email, presence: { message: "Please enter your email address"  }, length: { maximum: 100}
 
   # allow for 'not stated', male, female, and other
